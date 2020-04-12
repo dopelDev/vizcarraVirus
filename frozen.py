67d6d10919876f9ca3192d938fe04f0bab1a141d
dopel <322kuroneko@gmail.com>
2020-04-12 14:52:05 -0500

    12 april

--- frozen.py
+++ frozen.py
@@ -2,6 +2,7 @@ from index import app
 from flask_frozen import Freezer
 
 freezer = Freezer(app)
+app.config['FREEZER_DESTINATION'] = '/home/dopel/projects/vizcarraVirus/docs'
 
 if __name__ == '__main__':
     freezer.freeze()
